bac5ba37130f39cda4e81e3cd7938ffb3adbdc9d
Ethan-Liu301 <ethanliu123456789@outlook.com>
2024-10-06 20:21:08 -0700

    Update 1002.py

--- scripts/1002.py
+++ scripts/1002.py
@@ -1,3 +1,8 @@
-r = float(input())
-a = (r * r) * 3.14159 
-print("A=%0.4f" %a)
+while True:
+    try:
+        r = float(input("Give me any radius and I will calculate the circle's area: "))
+        a = (r * r) * 3.14159
+        print("A=%0.4f" % a)
+        break  # Exit the loop if the input is valid
+    except:
+        print("Please enter an actual number.")
